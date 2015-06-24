22b0113dbc05a7f7918e7c32c527587a4bba4c1f
Florian Pfitzer <pfitzer@w3p.cc>
2015-06-24 10:35:13 +0200

    config

--- src/js/config/config.js
+++ src/js/config/config.js
@@ -1,7 +1,7 @@
 var config = {
   // Defines the Marathon API URL,
   // leave empty to use the same as the UI is served.
-  apiURL: "http://localhost:8080/",
+  apiURL: "",
   // Intervall of API request in ms
   updateInterval: 5000,
   // Local http server URI while tests run
@@ -9,7 +9,7 @@ var config = {
     address: "localhost",
     port: 8181
   },
-  statsURL: "http://node01.mesos-cluster.local:3000/dashboard/db/containers?from=now-1h&to=now&var-node=All&var-container=All&var-marathon_app=<%= model.id %>&var-image=All"
+  statsURL: "http://" + window.location.hostname + ":3000/dashboard/db/containers?from=now-1h&to=now&var-node=All&var-container=All&var-marathon_app=<%= model.id %>&var-image=All"
 };
 
 module.exports = config;
